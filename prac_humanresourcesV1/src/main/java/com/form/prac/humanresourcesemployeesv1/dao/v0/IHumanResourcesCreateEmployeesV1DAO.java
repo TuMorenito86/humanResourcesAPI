5e2bceae6a825f1f30e288f97e61a7398f1d853b
AlfonsoSantiagoSampedroGarcia <alfonso.sampedro@accenture.com>
2025-02-14 12:33:35 +0100

    Creacion DAO
    Implementacion DAO

--- prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/dao/v0/IHumanResourcesCreateEmployeesV1DAO.java
+++ prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/dao/v0/IHumanResourcesCreateEmployeesV1DAO.java
@@ -1,5 +1,6 @@
 package com.form.prac.humanresourcesemployeesv1.dao.v0;
 
 public interface IHumanResourcesCreateEmployeesV1DAO {
-
+	
+	public void createEmployees();
 }
--- prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/dao/v0/impl/HumanResourcesCreateEmployeesV1.java
+++ prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/dao/v0/impl/HumanResourcesCreateEmployeesV1.java
@@ -1,5 +1,16 @@
 package com.form.prac.humanresourcesemployeesv1.dao.v0.impl;
 
-public class HumanResourcesCreateEmployeesV1 {
+import com.form.prac.humanresourcesemployeesv1.dao.v0.IHumanResourcesCreateEmployeesV1DAO;
 
+public class HumanResourcesCreateEmployeesV1 implements IHumanResourcesCreateEmployeesV1DAO{
+	
+	public HumanResourcesCreateEmployeesV1(){
+		super();
+	}
+	
+	@Override 
+	public void createEmployees(){
+		
+	}
+	
 }
