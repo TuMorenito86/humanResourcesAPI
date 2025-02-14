ef2f4011f602be8b035d9c672230bafa746e55cb
aresteban8 <aroa.romero@accenture.com>
2025-02-14 11:54:49 +0100

    Update HumanResourcesGetEmployeeV1Business.java
    impl business

--- prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/business/v1/impl/HumanResourcesGetEmployeeV1Business.java
+++ prac_humanresourcesV1/src/main/java/com/form/prac/humanresourcesemployeesv1/business/v1/impl/HumanResourcesGetEmployeeV1Business.java
@@ -2,22 +2,23 @@ package com.form.prac.humanresourcesemployeesv1.business.v1.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.form.prac.humanresourcesemployeesv1.business.v1.IHumanResourcesGetEmployeeV1Business;
 import com.form.prac.humanresourcesemployeesv1.business.v1.dtos.BDtoOutGetEmployeesGet;
+import com.form.prac.humanresourcesemployeesv1.dao.v1.IHumanResourcesGetByIdEmployeesV1DAO;
 
 @Component
 public class HumanResourcesGetEmployeeV1Business implements IHumanResourcesGetEmployeeV1Business {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HumanResourcesGetEmployeeV1Business.class);
-
+	@Autowired
+	private IHumanResourcesGetByIdEmployeesV1DAO daoSrv;
+	
 	@Override
 	public BDtoOutGetEmployeesGet humanResourcesEmployeesGetEmployeeV1(Long employeeId) {
-		LOGGER.info("humanResourcesEmployeesGetEmployeeV1 - in {}", employeeId);
-		BDtoOutGetEmployeesGet bDtoOut = null;
-		LOGGER.info("humanResourcesEmployeesGetEmployeeV1 - out {}", bDtoOut );
-		return bDtoOut;
+		return daoSrv.humanResourcesGetByIdEmployeesV1(employeeId);
 	}
 
 }
